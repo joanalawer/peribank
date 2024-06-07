816487cf12c74ba8c8e8d64925f621ee114160eb
joanalawer <lawerjoana477@gmail.com>
2024-06-07 00:13:41 +0000

    Updated server dot js

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const app = express();
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('pages'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
+// Importing routes
+const userRoutes = require('./routes/userRoutes');
+const accountRoutes = require('./routes/accountRoutes');
+
+app.use('/api', userRoutes);
+app.use('/api', accountRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/pages/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
