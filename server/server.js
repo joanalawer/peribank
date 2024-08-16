1be089d47914c925e862fa04868128ec1fe6eef7
Joana <lawerjoana477@gmail.com>
2024-08-16 22:32:33 +0000

    Added validation for registration form

--- server/server.js
+++ server/server.js
@@ -1,27 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const { Pool } = require('pg');
+const app = express();
+// const bodyParser = require('body-parser');
+// const { Pool } = require('pg');
+const { pool } = require('./db');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 const path = require('path');
-const app = express();
+
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'pages')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
-
-// // PostgreSQL connection setup
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'userdb',
-//     // password: 'admin',
-//     port: 5432,
-// });
 
 // Define routes
 app.get('/', (req, res) => {
@@ -80,36 +73,57 @@ app.get('/logout', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'index.html'));  // Serve the HTML file
 });
 
-  
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+app.post('/register', (req, res) => {
+    let { username, email, password, password2 } = req.body;
+    console.log({
+        username, email, password, password2
+    });
 
-    // Query to fetch user data
-    const query = 'SELECT * FROM users WHERE username = $1';
-    const values = [username];
-
-    try {
-        const result = await pool.query(query, values);
-
-        if (result.rows.length === 0) {
-            return res.status(401).send('Invalid Username or Password');
-        }
-
-        const user = result.rows[0];
-
-        // Compare the hashed password
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-            return res.status(401).send('Invalid Username or Password');
-        }
-
-        // If valid, redirect to the dashboard or desired page
-        res.redirect('/profile');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
+    let errors = [];
+    if (!username || !email || !password || !password2) {
+        errors.push({ message: 'Please enter all fields'});
     }
+    if (password.length < 8) {
+        errors.push({ message: 'Password should be at least 8 characters'});
+    }
+    if (password != password2) {
+        errors.push({ message: 'Passwords do not match'});
+    }
+    if (errors.length > 0) {
+        // Convert errors to a string and pass it as a query parameter
+        const query = errors.map(err => `message=${encodeURIComponent(err.message)}`).join('&');
+        res.redirect(`/register?${query}`);}
 });
+  
+// app.post('/login', async (req, res) => {
+//     const { username, password } = req.body;
+
+//     // Query to fetch user data
+//     const query = 'SELECT * FROM users WHERE username = $1';
+//     const values = [username];
+
+//     try {
+//         const result = await pool.query(query, values);
+
+//         if (result.rows.length === 0) {
+//             return res.status(401).send('Invalid Username or Password');
+//         }
+
+//         const user = result.rows[0];
+
+//         // Compare the hashed password
+//         const match = await bcrypt.compare(password, user.password);
+//         if (!match) {
+//             return res.status(401).send('Invalid Username or Password');
+//         }
+
+//         // If valid, redirect to the dashboard or desired page
+//         res.redirect('/profile');
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Server Error');
+//     }
+// });
 
 app.get('/profile', (req, res) => {
     res.send('Welcome to your dashboard!');
